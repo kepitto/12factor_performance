864b6ddbf499b13c3d6c94d177a4960f94692101
Hakan Kaya <hakan_kaya54@hotmail.com>
2022-11-03 21:32:28 +0100

    small changes and additional logging

--- database-seed.sql
+++ database-seed.sql
@@ -2,12 +2,7 @@ CREATE TABLE performanceData
 (
     id SERIAL,
     cam text,
-    cpu float,
+    cpu int,
     latency int,
     CONSTRAINT performanceData_pkey PRIMARY KEY (id)
 );
-
-INSERT INTO performanceData(cam, cpu, latency) VALUES
- ('cam1', '80', '100'),
- ('cam2', '80', '70'),
- ('cam1', '79', '98');
